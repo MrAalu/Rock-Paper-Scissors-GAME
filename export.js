e36baa91dd300bc8fdcc55cec9d6bcbf1fab3e38
MrAalu <lulamit16@gmail.com>
2023-01-31 12:03:14 +0530

    All Done

--- export.js
+++ export.js
@@ -1,6 +1,7 @@
 // AI Random Choice Generator
 
 export let aiPick;
+// computerChoose();
 export function computerChoose() {
   let num;
   num = Math.random() * 3;
@@ -19,16 +20,45 @@ export function computerChoose() {
       break;
   }
   document.getElementById("aipick").textContent = "AI Choose : " + aiPick;
-  //   console.log(aiPick);
   //   console.log(num);
+  //   console.log(aiPick);
 }
 
+// USER Picking ROCK - PAPER - Scissors
+let winLose;
+
+// User Choose ROCK
 export function rock() {
   document.getElementById("mypick").innerText = "You Choosed : Rock";
+
+  if (aiPick === "Rock") {
+    winLose = "Its a Draw";
+  } else {
+    winLose = aiPick === "Paper" ? "You Lost" : "You Win";
+  }
+  document.getElementById("result").innerText = "Result : " + winLose;
 }
+
+// User Choose Paper
 export function paper() {
   document.getElementById("mypick").innerText = "You Choosed : Paper";
+
+  if (aiPick === "Paper") {
+    winLose = "Its a Draw";
+  } else {
+    winLose = aiPick === "Scissors" ? "You Lost" : "You Win";
+  }
+  document.getElementById("result").innerText = "Result : " + winLose;
 }
+
+// User Choose Scissors
 export function scissors() {
   document.getElementById("mypick").innerText = "You Choosed : Scissors";
+
+  if (aiPick === "Scissors") {
+    winLose = "Its a Draw";
+  } else {
+    winLose = aiPick === "Rock" ? "You Lost" : "You Win";
+  }
+  document.getElementById("result").innerText = "Result : " + winLose;
 }
